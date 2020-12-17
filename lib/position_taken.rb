34ddf7c658aa526278bdb1a4bf95f394030ecad5
Firooz Mafakheri <firooz.mafakheri@gmail.com>
2020-12-17 13:17:34 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,8 @@
 # code your #position_taken? method here!
-defTic Tac Toe Position Taken(board,index)
+def position_taken(board,index)
   if board[index]==""|| board[index]==" " || board[index]==nil
     return false 
   else 
     return true
   end 
-end 
+end
